17899a1afab00bea5c32dc1dbe4e28545603b557
samuel <samusplay08@gmail.com>
2025-10-30 17:12:19 -0500

    menu creado con componentes

---
+++ src/app/pages/productos/productos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  imports: [],
+  templateUrl: './productos.html',
+  styleUrl: './productos.scss'
+})
+export class Productos {
+
+}
--- src/app/pages/sucursal/sucursal.ts
+++ src/app/pages/sucursal/sucursal.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sucursal',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './sucursal.html',
   styleUrl: './sucursal.scss'
 })
